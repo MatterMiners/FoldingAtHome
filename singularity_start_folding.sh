d9168d332021803f3ab71638a40a2c3b8a677cd1
Manuel Giffels <giffels@gmail.com>
2020-03-31 22:21:03 +0200

    Wait max 600 to start new WU

--- singularity_start_folding.sh
+++ singularity_start_folding.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-singularity exec /grid/fzk.de/mounts/nfs/software/cms/folding_at_home.img FAHClient --user=GridKa --team=250565 --gpu=false --smp=true --cpus=$TardisDroneCores --max-units=$[1*$TardisDroneCores] --exit-when-done=true
+singularity exec /grid/fzk.de/mounts/nfs/software/cms/folding_at_home.img FAHClient --user=GridKa --team=250565 --gpu=false --smp=true --cpus=$TardisDroneCores --max-units=$[1*$TardisDroneCores] --exit-when-done=true &
+
+PID=$!
+
+sleep 600 && grep "Running FahCore" log.txt || kill -9 ${PID}
+
+wait ${PID}
